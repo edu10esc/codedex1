cabd1d32547d3859503191233b181d69f398ede6
edu10esc <122527704+edu10esc@users.noreply.github.com>
2023-01-31 12:32:43 +0100

    Delete Ejer3_7.py

--- practica/Ejer3_7.py
+++
@@ -1,8 +0,0 @@
-num1=int(input('Introduzca un número: '))
-num2= int(input('Introduzca otro número: '))
-
-div1= num1/num2
-div2= num1//num2
-resto= div1-div2
-
-print('Decimales de la división: ', resto)
